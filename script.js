934cac1e30559cf133ad8987e6a8871faab5ec3e
Roberto Davila <davila.roberto17@csj-rpi.org>
2015-09-17 09:36:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function changeImage(){
   var image = document.getElementById('myImage');
   if(images.src.match("bulbon")){
-      image.src = "image/pic_bulboff.gif"
+      images.src = "image/pic_bulboff.gif"
   }else{
     images.src= "image/pic_bulbon.gif"
 }
